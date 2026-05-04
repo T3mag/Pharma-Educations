8a970158bab5bc420da683b4dc9dbb2adce1066c
Minnushin Artur <113981369+T3mag@users.noreply.github.com>
2026-05-04 15:14:45 +0300

    Добавление начального проекта приложения

---
+++ Pharma Education/Pharma Education/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pharma Education
+//
+//  Created by Артур Миннушин on 04.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
